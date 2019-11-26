866b305de8495b069aad1b24d7a1d6c661d003f1
8bit-1 <jruizgarcia40@gmail.com>
2019-11-26 13:47:28 -0600

    analizador lexico funcionnado

--- Codigo en C/Analizador lexico.c
+++ Codigo en C/Analizador lexico.c
@@ -1,45 +1,46 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//funciones
-void analizar(char c);
-void Estado();
-void Buscar();
+
+//prototipo de funciones
+void analizar(char c);  //funcion encargada de cambiar los estados o retornar error
+void Estado();			//verifica el estado y retorna su valor debido
+void Buscar();			//compara  identificador con palabrasReservadas, retorna la palabra si la encuentra, error de lo contrario
+
 //variable globales
 char *palabrasReservadas[] = {"definir","repetir","mientras","hasta","fin","finsi","si","imprimir"};
-int tamPalabrasReservadas=sizeof(palabrasReservadas)/sizeof(char *);
-int i;
-int estado=0;
 char identificador[100];
-char idNum[2];
-char temp[2];
+char idNum[2];				//aqui guardaremos el signo "-" para verificar si un numero es positivo o negativo
+char temp[2]; 			 //aqui guardaremos un caracter a la ves, y se concatena al array identificador
+int tamPalabrasReservadas=sizeof(palabrasReservadas)/sizeof(char *); //sizeof() devuelve el tamano en bytes que abarca la variable 
+int i;				 //maneja el numero de ciclos de la funcion
+int estado=0; 		 //el estado del programa que definira que esta permitido y que no
 
-//el lenguaje de programacion c: Denis Ritchie
 int main( )
 {
-   FILE *fp ;
+	char caracter;
+	FILE *fp ;
+	printf( "Opening the file test.c in read mode\n" ) ;
    
-   printf( "Opening the file test.c in read mode\n" ) ;
+	fp = fopen ( "data.txt", "r" ) ; // opening an existing file
    
-   fp = fopen ( "data.txt", "r" ) ; // opening an existing file
-   char caracter;
-      if ( fp == NULL )
-   {
+     if ( fp == NULL ){
      printf ( "Could not open file data.txt \n" ) ;
      return 1;
-   }
+	}
    printf( "Reading the file data.txt \n" ) ;
     
    while (1){
 	  caracter=fgetc(fp);
 	  if(caracter==EOF){break;}
+	 
 	  while(i==0){
 		   if(caracter!='{'){printf("impossible"); exit(-1);}
 		   i=1;
 		   }
 	  
 	  analizar(caracter);
+	  
 	  switch(caracter){
 		  case '{':printf("<Tkn_corcheteA>");break;
 		  case '}':printf("<Tkn_corcheteC>");break;
@@ -62,15 +63,18 @@ void buscar(){
 			}
 		if(i==(tamPalabrasReservadas)-1){printf("\nError no hay ID");exit(-1);}
 		}
-		
 	}
 
 void Estado(){
 	switch(estado){
+			//en el caso de ser un numero, analizamos si tiene un menos antes, con eso sabremos si es positivo o negativo
 			case 1: if(idNum[0]=='-'){printf("<Tkn_numeroNegativo>");idNum[0]='\0';}else printf("<Tkn_numero>"); estado=0;break;
 			case 2: if(idNum[0]=='-'){printf("<Tkn_numeroNegativo>");idNum[0]='\0';}else printf("<Tkn_numero>");estado=0;break;
-			case 4: if(idNum[0]=='-'){printf("<Tkn_decimalNegativo>");idNum[0]='\0';}else printf("<Tkn_numeroDecimal>");estado=0;break;
+			case 4: if(idNum[0]=='-'){printf("<Tkn_decimalNegativo>");idNum[0]='\0';}else printf("<Tkn_Decimal>");estado=0;break;
+			
+			//cuando detectamos un identificador, revisamos si esta en la tabla de palabras reservadas
 			case 5:buscar();estado=0; break;
+			
 			case 6:printf("<Tkn_variable>"); estado=0;break;
 			case 7:printf("<Tkn_variable>");estado=0;break;
 			case 9:printf("<Tkn_operadorRelacional>");estado=0;break;
@@ -82,16 +86,16 @@ void Estado(){
 			case 15:printf("<Tkn_MayorIgualQ>");estado=0;break;	
 			case 16:printf("<Tkn_OperadorMenos>");estado=0;break;	
 			case 18:printf("<Tkn_Cadena>");estado=0;break;	
-			
 			}
-	
 	}
 
 void analizar(char c){
 	
+	//comprobar el ultimo estado para devolver el Tkn adecuado
 	if(c==32||c=='}'||c=='{'||c=='('||c==')'||c==';'){
 		//printf("%i \n",estado);
-		Estado();
+		if(estado==17){estado=17;}else{
+		Estado();}
 		}
 	//Mayuscula
 	if( c>=65 && c<=90 ){
@@ -99,7 +103,7 @@ void analizar(char c){
 		else if(estado==17){estado=17;}else{printf("\n error");exit(-1);}
 		//printf("<Caracter Mayuscula>");
 		}
-	//Minuscula 97-122
+	//Minuscula 
 	if(c>=96 && c<=122){
 		if(estado==0){temp[0]=c; strcat(identificador,temp); estado=5;}else if(estado==6||estado==7){estado=7;}
 		else if(estado==5){temp[0]=c; strcat(identificador,temp);estado=5;}
@@ -111,14 +115,11 @@ void analizar(char c){
 		if(estado==0){estado=1;}else if(estado==1||estado==2){estado=2;}else if(estado==3||estado==4){estado=4;}
 		else if(estado==6||estado==7){estado=7;}else if(estado==16){estado=1;}
 		else if(estado==17){estado=17;}else{printf("\n errorN"); exit(-1);}
-	
-		//printf("<Caracter numeral>");
 		}
 	//punto decimal	
 	if(c=='.'){
 		if(estado==1||estado==2){estado=3;}
 		else if(estado==17){estado=17;}else{printf("\n error"); exit(-1);}
-		
 		 }
 	//signo "menos"
 	if(c=='-'){if(estado==0){estado=16; idNum[0]='-';}
@@ -129,32 +130,27 @@ void analizar(char c){
 		if(estado==0){estado=10;}else if(estado==17){estado=17;}else{printf("Error aritmetico");exit(-1);}
 		
 		}
-	//operador relacional
-	
-	//~ if(c=='='||c=='<'||c=='>'||c=='!'){
-		
-		//~ //printf("<operador relacional>");
-		//~ }
-		
+	//operador menor que
 	if(c=='<'){                
 	
 		if(estado==0){estado=11;}else if(estado==17){estado=17;}else{printf("error <");}
 		}
+	//operador mayor que
 	if(c=='>'){   
 		if(estado==0){estado=14;}else if(estado==17){estado=17;}else{printf("error >");}
 		 }	
-	
+	//operador igual
 	if(c=='='){
 		 if(estado==0){estado=8;}
 		 else if(estado==8){estado=9;}else if(estado==11){estado=13;}
 		 else if(estado==14){estado=15;}
 		 else if(estado==17){estado=17;}else{printf("error =");exit(-1);}
 		}
-		
+	//operador distinto
 	if(c=='!'){
 		if(estado==0){estado=8;}else if(estado==17){estado=17;}else{printf("\n error !");exit(-1);} 
 		}
-		
+	//comillas
 	if(c=='"'){
 		if(estado==0){estado=17;}else if(estado==17){estado=18;}else{printf("error \" ");}
 		
